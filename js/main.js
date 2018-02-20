1e88974a56e380dfc1dbb3e777e5b4d152dac359
victor-mwania <victormwnza@gmail.com>
2018-02-20 18:33:06 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+var $colors=$(['#3A4B5B','#9B5094','#99C436', '#2562A8','#84221D','#33EDF7', '#93821F','6A605C','#B9EABA' ]);
+$(document).ready(function() {
+    loadQuote();
+});
+ function loadQuote(){
+    $("#quote_body").empty();
+    $("#quote_author").empty();
+    var api="H2YE0mH3wKmshcDPCpyWTueHAXW8p1MTkozjsniSjRwYHwiQSS";
+    var url="https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=10";
+    
+    $.getJSON(url, {"mashape-key":api}, function(response){
+        var $text=response.quote;
+        var $qoute_author=response.author;
+        console.log($text);
+        console.log($qoute_author);
+
+        $("#quote_body").append($text);
+        $("#quote_author").append($qoute_author);
+    });
+    $('#tweet-quote').on('click', function() {
+          openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + $text + '" ' + $qoute_author));
+        });
+}
+
+$("#new_quote").click(function(){loadQuote()});
